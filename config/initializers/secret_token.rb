0a12392719ee8474e3e17034a62314e43d04ced3
jennikins813 <jennikins813@yahoo.com>
2014-05-01 17:20:50 -0400

    Initial commit_rails practice

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railsgirls::Application.config.secret_key_base = '4f248a62342f7563ed9c430c32112df885f70c48dcf7cdbfd76434aa9b9c7d10e588e567798018401d0feaae3dae2179aa1f445d8ff725b020fecb212b101020'
