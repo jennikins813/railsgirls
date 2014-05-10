060b7b99697339080a0986a2f484aac8cde43841
jennikins813 <jennikins813@yahoo.com>
2014-05-09 23:22:13 -0400

    Render form to add comments to ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -16,6 +16,15 @@ class IdeasController < ApplicationController
   # GET /ideas/1
   # GET /ideas/1.json
   def show
+    @idea = Idea.find(params[:id])
+
+    @comments = @idea.comments.all
+    @comment = @idea.comments.build
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @idea }
+    end
   end
 
   # GET /ideas/new
