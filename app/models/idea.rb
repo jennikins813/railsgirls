83732315ea4a2a84fe44291f21ba3106f2e32163
jennikins813 <jennikins813@yahoo.com>
2014-05-09 23:02:48 -0400

    Added associations for comments and ideas

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,6 @@
 class Idea < ActiveRecord::Base
-	#attr_accessible :tag_list
+	has_many :comments
+	
 	mount_uploader :picture, PictureUploader
 	#add the uploader with the column :picture and the class name PictureUploader
 	acts_as_taggable
